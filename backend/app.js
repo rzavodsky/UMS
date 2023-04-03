783d7a15fa2f90c2568fce28e831195fdddbdc96
Tracktark <rikoczz@gmail.com>
2023-04-03 22:36:54 +0200

    Set up basic express API

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,12 @@
-console.log("Hello world")
+import express, { json } from 'express'
+
+const port = 3000
+const app = express()
+const router = express.Router()
+
+app.use(json())
+app.use('/api', router)
+
+app.listen(port, () => {
+    console.log(`UMS backend listening on port ${port}`)
+})
