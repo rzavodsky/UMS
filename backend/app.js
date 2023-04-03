76ad31d57dbae14896b8b84191490c4efbdead02
Tracktark <rikoczz@gmail.com>
2023-04-03 22:37:30 +0200

    Added validation

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express, { json } from 'express'
+import { ValidationError } from 'express-json-validator-middleware'
 
 const port = 3000
 const app = express()
@@ -6,6 +7,15 @@ const router = express.Router()
 
 app.use(json())
 app.use('/api', router)
+
+app.use((err, _req, res, next) => {
+    if (err instanceof ValidationError) {
+        res.status(400).send(err.validationErrors)
+        next()
+    } else {
+        next(err)
+    }
+})
 
 app.listen(port, () => {
     console.log(`UMS backend listening on port ${port}`)
---
+++ backend/validation.js
@@ -0,0 +1,24 @@
+
+/// A middleware, which tries to convert all params to integers.
+/// If any param can't be converted, sends a response with an error message.
+export function validateIdParams(req, res, next) {
+    const errors = []
+    for (let key in req.params) {
+        req.params[key] = parseInt(req.params[key], 10)
+        if (isNaN(req.params[key])) {
+            errors.push({
+                key,
+                message: `Param '${key}' needs to be an integer`,
+            })
+        }
+    }
+
+    if (errors.length !== 0) {
+        return res.status(400).json({
+            error: "Param Validation Error",
+            messages: errors
+        })
+    }
+    next()
+
+}
