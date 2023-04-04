3b5725528687ec888838b4fca254257c81d8967b
Tracktark <rikoczz@gmail.com>
2023-04-04 22:19:45 +0200

    Fixed id param validation

--- backend/validation.js
+++ backend/validation.js
@@ -4,6 +4,7 @@
 export function validateIdParams(req, res, next) {
     const errors = []
     for (let key in req.params) {
+        if (!key.toLocaleLowerCase().includes("id")) continue
         req.params[key] = parseInt(req.params[key], 10)
         if (isNaN(req.params[key])) {
             errors.push({
@@ -15,7 +16,7 @@ export function validateIdParams(req, res, next) {
 
     if (errors.length !== 0) {
         return res.status(400).json({
-            error: "Param Validation Error",
+            error: "Validation Error",
             messages: errors
         })
     }
