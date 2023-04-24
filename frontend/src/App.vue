f179481b391272cfeb1f2cbd427efe5771d79ec7
Tracktark <rikoczz@gmail.com>
2023-04-24 14:38:27 +0200

    Added Vue router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { RouterView, RouterLink } from 'vue-router'
+import { useLoginStore } from './stores/login';
+const login = useLoginStore();
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <RouterView />
+    </main>
 </template>
 
 <style scoped>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,40 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import { useLoginStore } from './stores/login'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
+const login = useLoginStore()
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => {
+                if (login.user?.isAdmin) {
+                    return import("@/views/Admin.vue")
+                } else if (login.user?.isTeacher) {
+                    return import("@/views/Teacher.vue")
+                } else {
+                    return import("@/views/Student.vue")
+                }
+            }
+        },
+        { path: '/login', component: Login, name: 'login' },
+    ]
+});
+router.beforeEach((to, _from) => {
+    if (!login.loggedIn && to.name !== 'login') {
+        return { name: 'login' };
+    }
+});
+app.use(router)
 
 app.mount('#app')
+
